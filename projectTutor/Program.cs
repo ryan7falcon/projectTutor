6f5fb4ac37a2ac69617676068d2c24bd7304e81e
javel <javel@DESKTOP-HS6MCF4>
2016-07-13 10:06:50 -0700

    First commit

--- projectTutor/Program.cs
+++ projectTutor/Program.cs
@@ -11,6 +11,7 @@ namespace projectTutor
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// Test
         [STAThread]
         static void Main()
         {
